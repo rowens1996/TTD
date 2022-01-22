641a23cbad3439344d8d0f93b6fb0abb5a101359
Ryan <96872747+rowens1996@users.noreply.github.com>
2022-01-22 09:38:09 +0000

    On branch main 	modified:   babel.config.js 	modified:   index.js 	modified:   index.spec.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//Inital set-up
+//Game of Life Rules
+//fewer than 2 neighbours the cell dies
+//more than 3 neighbours the cell dies
+//2 or 3 neighbours the cell lives
+//any dead cell with exactly 3 neighbours becomes a live cell 
+
+function gameOfLife(neighbours) {
+    return false;
+}
